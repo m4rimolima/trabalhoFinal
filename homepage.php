d2bcca8e228cf2f2d7dce446484bea479e9800c7
m4rimolima <m4rimolima@gmail.com>
2025-06-25 17:23:47 -0300

    adicionando os banners

--- homepage.php
+++ homepage.php
@@ -1,7 +1,7 @@
-
 <?php
 session_start();
 include('protect.php');
+include('includes/menu.php');
 ?>
 
 <!DOCTYPE html>
@@ -10,21 +10,22 @@ include('protect.php');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ULTRAVIOLET BOOKSTORE</title>
-    <link rel="stylesheet" href="assets/style.css">
-    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap" rel="stylesheet">
 
+    <link rel="stylesheet" href="/trabalhofinal/assets/homepage.css">
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap" rel="stylesheet">
 </head>
 <body class="homepage">
-    <h1 class="h1Homepage">HOME PAGE</h1>
-    Welcome to the home page, <?php echo htmlspecialchars($_SESSION['name']); ?>!
-    <p>We're so sorry, but this page isn't ready yet!</p>
-    <br><br>
-    <a href="logout.php" class="logoutButton">Logout</a>
-    <a href="/trabalhofinal/crud1/listbooks.php">
-        <button type="button">Cadastrar Novo Livro</button>
-    </a>
+    <p class="welcomeMessage">
+        Welcome to the home page, <?php echo htmlspecialchars($_SESSION['name']); ?>!
+    </p>
 
-</form>
+    <section class="homepageBanner">
+        <img src="/trabalhofinal/assets/images/homepageBanner.png" />
+    </section>
+    <section class="subBanner">
+        <img src="/trabalhofinal/assets/images/subBanner.png" />
+    </section>
+    <section class="booksBanner">
+        <img src="/trabalhofinal/assets/images/booksBanner.png" />
+    </section>
 
-</body>
-</html>
